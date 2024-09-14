78a6de992207cd220c9f7ca3ec0e41a532b1d082
santiRendon <xantirendonc@gmail.com>
2024-09-14 18:58:51 -0500

    Add Footer and Navbar components

---
+++ frontend/src/lib/Navbar.svelte
@@ -0,0 +1,42 @@
+<script>
+  import {
+    Header,
+    HeaderNav,
+    HeaderNavItem,
+    SideNav,
+    SideNavItems,
+    SideNavLink,
+    SideNavDivider,
+    SkipToContent,
+  } from "carbon-components-svelte";
+
+  let isSideNavOpen = false;
+</script>
+
+<Header
+  expansionBreakpoint={1700}
+  company="Ecofound"
+  platformName="ICP"
+  bind:isSideNavOpen
+>
+  <svelte:fragment slot="skip-to-content">
+    <SkipToContent />
+  </svelte:fragment>
+  <HeaderNav>
+    <HeaderNavItem href="/" text="About" />
+    <HeaderNavItem href="/" text="Help" />
+    <HeaderNavItem href="/" text="Start a project" />
+  </HeaderNav>
+</Header>
+
+<SideNav bind:isOpen={isSideNavOpen}>
+  <SideNavItems>
+    <SideNavLink text="About" />
+    <SideNavLink text="Help" />
+    <SideNavLink text="Start a project" />
+    <SideNavDivider />
+  </SideNavItems>
+</SideNav>
+
+<style>
+</style>
---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import "carbon-components-svelte/css/all.css"; // https://github.com/carbon-design-system/carbon-components-svelte?tab=readme-ov-file
+  import Navbar from "$lib/Navbar.svelte";
+  import Footer from "$lib/Footer.svelte";
+</script>
+
+<Navbar />
+<slot />
+<Footer />
+
+<style>
+  * {
+    padding: 0px;
+    margin: 0px;
+  }
+</style>
