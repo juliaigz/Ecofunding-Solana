7f7a2857f44b86fc9e258439a730f147d8ed08ea
santiRendon <xantirendonc@gmail.com>
2024-09-14 19:09:11 -0500

    Add CSS optimization plugin to Vite configuration

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { optimizeCss } from "carbon-preprocess-svelte";
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit(), optimizeCss()]
 });
