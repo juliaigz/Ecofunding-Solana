fbd404059f8e5642976a386a3fe4883ede9bba37
santiRendon <xantirendonc@gmail.com>
2024-09-14 19:09:19 -0500

    Add carbon-icons-svelte and carbon-pictograms-svelte dependencies

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script>
-  import "carbon-components-svelte/css/all.css"; // https://github.com/carbon-design-system/carbon-components-svelte?tab=readme-ov-file
-  import Navbar from "$lib/Navbar.svelte";
-  import Footer from "$lib/Footer.svelte";
+	import 'carbon-components-svelte/css/all.css'; // https://github.com/carbon-design-system/carbon-components-svelte?tab=readme-ov-file
+	import Navbar from '$lib/Navbar.svelte';
+	import Footer from '$lib/Footer.svelte';
+
+	// https://carbon-icons-svelte.onrender.com/
+	// https://carbon-pictograms-svelte.onrender.com/
 </script>
 
 <Navbar />
@@ -9,8 +12,8 @@
 <Footer />
 
 <style>
-  * {
-    padding: 0px;
-    margin: 0px;
-  }
+	* {
+		padding: 0px;
+		margin: 0px;
+	}
 </style>
