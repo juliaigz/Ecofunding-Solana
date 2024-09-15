faf42393233a8332a97a180dc40e7259692444d2
santiRendon <xantirendonc@gmail.com>
2024-09-14 19:19:58 -0500

    Formart all code using prettier and eslint

--- frontend/src/lib/Navbar.svelte
+++ frontend/src/lib/Navbar.svelte
@@ -1,41 +1,36 @@
 <script>
-  import {
-    Header,
-    HeaderNav,
-    HeaderNavItem,
-    SideNav,
-    SideNavItems,
-    SideNavLink,
-    SideNavDivider,
-    SkipToContent,
-  } from "carbon-components-svelte";
+	import {
+		Header,
+		HeaderNav,
+		HeaderNavItem,
+		SideNav,
+		SideNavItems,
+		SideNavLink,
+		SideNavDivider,
+		SkipToContent
+	} from 'carbon-components-svelte';
 
-  let isSideNavOpen = false;
+	let isSideNavOpen = false;
 </script>
 
-<Header
-  expansionBreakpoint={1700}
-  company="Ecofound"
-  platformName="ICP"
-  bind:isSideNavOpen
->
-  <svelte:fragment slot="skip-to-content">
-    <SkipToContent />
-  </svelte:fragment>
-  <HeaderNav>
-    <HeaderNavItem href="/" text="About" />
-    <HeaderNavItem href="/" text="Help" />
-    <HeaderNavItem href="/" text="Start a project" />
-  </HeaderNav>
+<Header expansionBreakpoint={1700} company="Ecofound" platformName="ICP" bind:isSideNavOpen>
+	<svelte:fragment slot="skip-to-content">
+		<SkipToContent />
+	</svelte:fragment>
+	<HeaderNav>
+		<HeaderNavItem href="/" text="About" />
+		<HeaderNavItem href="/" text="Help" />
+		<HeaderNavItem href="/" text="Start a project" />
+	</HeaderNav>
 </Header>
 
 <SideNav bind:isOpen={isSideNavOpen}>
-  <SideNavItems>
-    <SideNavLink text="About" />
-    <SideNavLink text="Help" />
-    <SideNavLink text="Start a project" />
-    <SideNavDivider />
-  </SideNavItems>
+	<SideNavItems>
+		<SideNavLink text="About" />
+		<SideNavLink text="Help" />
+		<SideNavLink text="Start a project" />
+		<SideNavDivider />
+	</SideNavItems>
 </SideNav>
 
 <style>
