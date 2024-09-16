7babc70af314ecaed7f64187df22029baba2ba77
santiRendon <xantirendonc@gmail.com>
2024-09-15 23:09:46 -0500

    Refactor Footer and Navbar components

--- frontend/src/lib/Footer.svelte
+++ frontend/src/lib/Footer.svelte
@@ -1,7 +1,7 @@
 <script>
 	import { Grid, Row, Column } from 'carbon-components-svelte';
 	import imgFooterBurbujas from '$lib/images/imgFooterBurbujas.png';
-	import { Button } from 'carbon-components-svelte';
+	import { Button, UnorderedList, ListItem, Link } from 'carbon-components-svelte';
 	import Add from 'carbon-icons-svelte/lib/Add.svelte';
 </script>
 
@@ -16,12 +16,20 @@
 			<p>Ecofunding <strong>SOL</strong></p>
 		</Column>
 		<Column style="margin-top: 5%;">
-			<ul>
-				<li>ABOUT</li>
-				<li>HELP</li>
-				<li>START A PROJECT</li>
-				<li>CONTACT US</li>
-			</ul>
+			<UnorderedList expressive style="padding: 5%;">
+				<ListItem style="padding: 3%;">
+					<Link size="lg" href="/" style="color:black; ">About</Link></ListItem
+				>
+				<ListItem style="padding: 3%;">
+					<Link size="lg" href="/help" style="color:black; ">Help</Link></ListItem
+				>
+				<ListItem style="padding: 3%;"
+					><Link size="lg" href="/project" style="color:black; ">START A PROJECT</Link></ListItem
+				>
+				<ListItem style="padding: 3%;"
+					><Link size="lg" href="/contact" style="color:black; ">CONTACT US</Link></ListItem
+				>
+			</UnorderedList>
 		</Column>
 		<Column style="margin-top: 5%;">
 			<Button icon={Add} style="background-color:#59CF8C; width: 12.625rem;">find a project</Button>
@@ -33,9 +41,5 @@
 	hr {
 		margin-top: 12%;
 		width: 100%;
-	}
-
-	ul li {
-		padding: 5%;
 	}
 </style>
--- frontend/src/lib/Navbar.svelte
+++ frontend/src/lib/Navbar.svelte
@@ -3,17 +3,27 @@
 		Header,
 		HeaderNav,
 		HeaderNavItem,
+		HeaderUtilities,
+		HeaderGlobalAction,
 		SideNav,
 		SideNavItems,
 		SideNavLink,
 		SideNavDivider,
-		SkipToContent
+		SkipToContent,
+		Button
 	} from 'carbon-components-svelte';
+	import { UserAvatarFilledAlt, SettingsAdjust, Logout } from 'carbon-icons-svelte';
 
 	let isSideNavOpen = false;
 </script>
 
-<Header expansionBreakpoint={1700} company="Ecofunding" platformName="SOL" bind:isSideNavOpen>
+<Header
+	expansionBreakpoint={1700}
+	company="Ecofunding"
+	platformName="SOL"
+	bind:isSideNavOpen
+	href="/"
+>
 	<svelte:fragment slot="skip-to-content">
 		<SkipToContent />
 	</svelte:fragment>
@@ -22,6 +32,12 @@
 		<HeaderNavItem href="/help" text="Help" />
 		<HeaderNavItem href="/project" text="Start a project" />
 	</HeaderNav>
+	<HeaderUtilities>
+		<!-- <HeaderGlobalAction iconDescription="Settings" tooltipAlignment="start" icon={SettingsAdjust} /> -->
+		<!-- <HeaderGlobalAction iconDescription="Profile" icon={UserAvatarFilledAlt} /> -->
+		<!-- <HeaderGlobalAction iconDescription="Log out" tooltipAlignment="end" icon={Logout} /> -->
+		<Button style="background-color:#59CF8C;">Connect Wallet</Button>
+	</HeaderUtilities>
 </Header>
 
 <SideNav bind:isOpen={isSideNavOpen}>
