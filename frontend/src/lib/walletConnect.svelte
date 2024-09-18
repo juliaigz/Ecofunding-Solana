72796096a1f260c0ff5a1fc254705591bd822918
santiRendon <xantirendonc@gmail.com>
2024-09-18 15:44:39 -0500

    wallet connect

--- frontend/src/lib/walletConnect.svelte
+++ frontend/src/lib/walletConnect.svelte
@@ -1,10 +1,10 @@
-<script>
+<script lang="ts">
 	import { Button } from 'carbon-components-svelte';
 	import { PhantomWalletAdapter } from '@solana/wallet-adapter-wallets';
 	import { Connection, clusterApiUrl } from '@solana/web3.js';
 	import { onMount } from 'svelte';
 
-	let wallet = null;
+	let wallet: PhantomWalletAdapter;
 	let connected = false;
 	let publicKey = '';
 
@@ -16,6 +16,7 @@
 		// Inicializar la conexión a Solana y la wallet Phantom
 		connection = new Connection(endpoint);
 		wallet = new PhantomWalletAdapter();
+		console.log(wallet); // Installed | NotDetected | Loadable | Unsupported
 	});
 
 	// Función para conectar la wallet Phantom
@@ -23,20 +24,33 @@
 		try {
 			await wallet.connect();
 			connected = true;
-			publicKey = wallet.publicKey.toString();
+			// @ts-ignore
+			publicKey = wallet.publicKey?.toString();
 			console.log('Conectado con la dirección:', publicKey);
 		} catch (error) {
 			console.error('Error al conectar la wallet:', error);
 		}
 	}
+
+	async function disconnectWallet() {
+		try {
+			if (wallet.connected) {
+				await wallet.disconnect();
+				connected = false;
+				console.log('Wallet desconectada');
+			} else {
+				console.log('La wallet ya está desconectada');
+			}
+		} catch (error) {
+			console.error('Error al desconectar la wallet:', error);
+		}
+	}
 </script>
 
 <!-- Botón para conectar la wallet -->
-<Button on:click={connectWallet} style="background-color:#59CF8C;">
-	{#if connected}
-		Wallet Connected: {publicKey}
-	{/if}
-	{#if !connected}
-		Connect Wallet
-	{/if}
-</Button>
+
+#{#if connected}
+	<Button on:click={disconnectWallet} style="background-color:#59CF8C;">Disconnect Wallet</Button>
+{:else}
+	<Button on:click={connectWallet} style="background-color:#59CF8C;">Connect Wallet</Button>
+{/if}
