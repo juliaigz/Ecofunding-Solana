e03823cc6792d11b75487c23072496dff959ba28
santiRendon <xantirendonc@gmail.com>
2024-09-18 16:43:07 -0500

    ✨ added ReadStoreExample component to demonstrate store reading

---
+++ frontend/src/lib/ReadStoreExample.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { walletAddress } from '$lib/stores'; // Importa el store
+
+	let address: string;
+
+	// Suscribirse al store para obtener la dirección de la wallet
+	walletAddress.subscribe((value) => {
+		address = value;
+	});
+</script>
+
+<p>Dirección de la wallet: {address}</p>
