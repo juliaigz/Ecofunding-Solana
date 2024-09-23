2886103cbc4b4158d26329d37c0ea69d4b82b7cc
santiRendon <xantirendonc@gmail.com>
2024-09-23 16:40:14 -0500

    ⚡️dynamically import DriftClient initialization on button click

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,15 +1,18 @@
 <script>
-	// import { initializeDriftClient } from '$lib/services/drift.client';
-	// import { Button } from 'carbon-components-svelte';
+	import { initializeDriftClient } from '$lib/services/drift.client';
+	import { Button } from 'carbon-components-svelte';
 
-	// let init = async () => {
-	// 	let i = await initializeDriftClient();
-	// 	// console.log(i);
-	// 	return i;
-	// };
+	let init = async () => {
+		const { initializeDriftClient } = await import('$lib/services/drift.client');
+		let i = await initializeDriftClient();
+		console.log(i);
+		return i;
+	};
+
+	// no import() -> charge in 41s
 </script>
 
-<!-- <Button on:click={init}>Test</Button> -->
+<Button on:click={init}>Test</Button>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
