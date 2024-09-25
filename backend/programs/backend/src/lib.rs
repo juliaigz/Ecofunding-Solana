244eab6f85407f724071b311e53db315a14b7c0a
Aliro Albanez <aliro.albanez@gmail.com>
2024-09-25 12:18:25 -0300

    Cambios en el codigo de transacciones de sol

--- backend/programs/backend/src/lib.rs
+++ backend/programs/backend/src/lib.rs
@@ -1,5 +1,7 @@
+#![allow(clippy::result_large_err)]
+
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::system_instruction; // Importa las instrucciones del sistema
+use anchor_lang::solana_program::{system_program, pubkey::Pubkey}; // Importa las instrucciones del sistema
 
 declare_id!("684gruPz6pRymjJz9LZ9kuRd85hmwdhGo9TUNEre6BEV");
 
@@ -7,40 +9,34 @@ declare_id!("684gruPz6pRymjJz9LZ9kuRd85hmwdhGo9TUNEre6BEV");
 pub mod backend {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
-        Ok(())
-    }
-
     pub fn transfer_sol(ctx: Context<Transfer>, amount: u64) -> Result<()> {
         // Lógica para transferir SOL
-        let cpi_accounts = system_instruction::Transfer {
-            from: *ctx.accounts.from.key,
-            to: *ctx.accounts.to.key,
-        };
-        let cpi_program = ctx.accounts.system_program.to_account_info();
-        let ix = system_instruction::transfer(&ctx.accounts.from.key(), &ctx.accounts.to.key(), amount);
-        anchor_lang::solana_program::program::invoke(
-            &ix,
-            &[
-                ctx.accounts.from.to_account_info(),
-                ctx.accounts.to.to_account_info(),
-                cpi_program,
-            ],
-        )?;
+        let payer = &mut ctx.accounts.payer;
+        let recipient = &mut ctx.accounts.recipient;
+
+        let lamports = payer.lamports();
+        if lamports < amount {
+            return Err(ErrorCode::InsufficientFunds.into());
+        }
+
+        **payer.try_borrow_mut_lamports()? -= amount;
+        **recipient.try_borrow_mut_lamports()? += amount;
+
         Ok(())
     }
 }
 
-#[derive(Accounts)]
-pub struct Initialize {}
 
 #[derive(Accounts)]
 pub struct Transfer<'info> {
     #[account(mut)]
-    pub from: Signer<'info>, // La cuenta de origen (debe ser firmante)
+    payer: Signer<'info>, // La cuenta de origen (debe ser firmante)
     #[account(mut)]
-    pub to: AccountInfo<'info>, // La cuenta de destino
-    pub system_program: Program<'info, System>, // El programa del sistema
+    recipient: AccountInfo<'info>, // La cuenta de destino
 }
 
+#[error]
+pub enum ErrorCode {
+    #[msg("Insufficient funds in payer's account")]
+    InsufficientFunds,
+}
